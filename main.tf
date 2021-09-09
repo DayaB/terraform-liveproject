8b14bf0871c02a52194d6f56fd4320194b96b3f5
Daya Bitra <dayanandam@yahoo.com>
2021-09-08 20:27:37 -0500

    Initial Terraform code

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region  = "us-east-1"
+}
+ 
+resource "aws_instance" "helloworld" {
+  ami           = "ami-087c17d1fe0178315"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "HelloWorld"
+  }
